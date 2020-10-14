f85b8cbce703472562a520e15333012053a63b9b
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2020-10-14 17:49:57 -0400

    Add files via upload

---
+++ pro.v2.py
@@ -0,0 +1,47 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.core.window import Window
+#from pynput import keyboard
+
+Window.clearcolor = (0, 0, 0.1, 0.9)
+#Window.size = (500, 500)
+
+
+class login(Screen):
+    password = ObjectProperty(None)
+    def __init__(self, **kwargs):
+        super(login, self).__init__(**kwargs)
+
+
+    def submit(self):
+        if self.password.text != "":
+            self.reset()
+    def reset(self):
+        self.password.text = ""
+
+class MainWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+kv = Builder.load_file("pv2.kv")
+sm = WindowManager()
+
+screens = [login(name="login"), MainWindow(name="main")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "login"
+
+class Project(App):
+    def build(self):
+        return sm
+
+
+
+if __name__ == "__main__":
+    Project().run()
