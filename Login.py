d3744931b377514238406b09f105dcacf6e1ac65
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2020-10-14 18:13:24 -0400

    Update and rename pro.v2.py to Login.py

--- Login.py
+++ Login.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
-from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.core.window import Window
 
